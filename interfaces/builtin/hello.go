a2bd3b571b47d36db07a531f77772e093e5e059b
Zygmunt Krynicki <me@zygoon.pl>
2016-08-09 18:23:55 +0200

    Interfaces/builtin: add the new hello interface
    This patch adds a dummy new interface “hello” that does nothing at all.

---
+++ interfaces/builtin/hello.go
@@ -0,0 +1,132 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2016 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package builtin
+
+import (
+	"fmt"
+
+	"github.com/snapcore/snapd/interfaces"
+)
+
+// HelloInterface is the hello interface for a tutorial.
+type HelloInterface struct{}
+
+// String returns the same value as Name().
+func (iface *HelloInterface) Name() string {
+	return "hello"
+}
+
+// SanitizeSlot checks and possibly modifies a slot.
+func (iface *HelloInterface) SanitizeSlot(slot *interfaces.Slot) error {
+	if iface.Name() != slot.Interface {
+		panic(fmt.Sprintf("slot is not of interface %q", iface))
+	}
+	// NOTE: currently we don't check anything on the slot side.
+	return nil
+}
+
+// SanitizePlug checks and possibly modifies a plug.
+func (iface *HelloInterface) SanitizePlug(plug *interfaces.Plug) error {
+	if iface.Name() != plug.Interface {
+		panic(fmt.Sprintf("plug is not of interface %q", iface))
+	}
+	// NOTE: currently we don't check anything on the plug side.
+	return nil
+}
+
+// ConnectedSlotSnippet returns security snippet specific to a given connection between the hello slot and some plug.
+func (iface *HelloInterface) ConnectedSlotSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// PermanentSlotSnippet returns security snippet permanently granted to hello slots.
+func (iface *HelloInterface) PermanentSlotSnippet(slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// ConnectedPlugSnippet returns security snippet specific to a given connection between the hello plug and some slot.
+func (iface *HelloInterface) ConnectedPlugSnippet(plug *interfaces.Plug, slot *interfaces.Slot, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// PermanentPlugSnippet returns the configuration snippet required to use a hello interface.
+func (iface *HelloInterface) PermanentPlugSnippet(plug *interfaces.Plug, securitySystem interfaces.SecuritySystem) ([]byte, error) {
+	switch securitySystem {
+	case interfaces.SecurityAppArmor:
+		return nil, nil
+	case interfaces.SecuritySecComp:
+		return nil, nil
+	case interfaces.SecurityDBus:
+		return nil, nil
+	case interfaces.SecurityUDev:
+		return nil, nil
+	case interfaces.SecurityMount:
+		return nil, nil
+	default:
+		return nil, interfaces.ErrUnknownSecurity
+	}
+}
+
+// AutoConnect returns true if plugs and slots should be implicitly
+// auto-connected when an unambiguous connection candidate is available.
+//
+// This interface does not auto-connect.
+func (iface *HelloInterface) AutoConnect() bool {
+	return false
+}
