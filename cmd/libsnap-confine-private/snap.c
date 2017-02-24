d84179e03f30b2feb4875d91b85e522c01bf5009
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2017-02-24 23:34:37 +0100

    cmd/libsnap: add function for validating snap names
    This patch adds a function that can be used to validate snap names. Due
    to the differences between go and libc regular expression engine the
    same expression cannot be used but care has been taken to make sure it
    has the same meaning.
    
    The only difference is that the syntax "(?:)" becomes just "()"
    
    Signed-off-by: Zygmunt Krynicki <zygmunt.krynicki@canonical.com>

--- cmd/libsnap-confine-private/snap.c
+++ cmd/libsnap-confine-private/snap.c
@@ -17,11 +17,14 @@
 #include "config.h"
 #include "snap.h"
 
+#include <regex.h>
 #include <stddef.h>
 #include <stdlib.h>
-#include <regex.h>
+#include <string.h>
+#include <errno.h>
 
 #include "../libsnap-confine-private/utils.h"
+#include "../libsnap-confine-private/string-utils.h"
 
 bool verify_security_tag(const char *security_tag)
 {
@@ -42,4 +45,40 @@ bool verify_security_tag(const char *security_tag)
 	regfree(&re);
 
 	return (status == 0);
+}
+
+void sc_snap_name_validate(const char *snap_name, struct sc_error **errorp)
+{
+	struct sc_error *err = NULL;
+
+	const char *name_re = "^([a-z0-9]+-?)*[a-z](-?[a-z0-9])*$";
+
+	regex_t re;
+	if (regcomp(&re, name_re, REG_EXTENDED | REG_NOSUB) != 0) {
+		die("cannot compile regex %s", name_re);
+	}
+
+	int status = regexec(&re, snap_name, 0, NULL, 0);
+	regfree(&re);
+
+	if (status != 0) {
+		size_t quote_buf_size = strlen(snap_name) * 4 + 3;
+		char *quote_buf = calloc(1, quote_buf_size);
+
+		if (quote_buf == NULL) {
+			err =
+			    sc_error_init_from_errno(errno,
+						     "cannot allocate memory for quoted name");
+			goto out;
+		}
+
+		sc_string_quote(quote_buf, quote_buf_size, snap_name);
+		err =
+		    sc_error_init(SC_SNAP_DOMAIN, SC_SNAP_INVALID_NAME,
+				  "invalid snap name %s", quote_buf);
+		free(quote_buf);
+	}
+
+ out:
+	sc_error_forward(errorp, err);
 }
