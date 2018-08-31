cb919b745b9b3da2ba8bec62e5ec18cfda039228
Zygmunt Krynicki <me@zygoon.pl>
2018-08-31 15:07:23 +0200

    poke holes for symlinks

--- cmd/snap-update-ns/change.go
+++ cmd/snap-update-ns/change.go
@@ -156,7 +156,7 @@ func (c *Change) ensureTarget(sec *Secure) ([]*Change, error) {
 		case "symlink":
 			if fi.Mode()&os.ModeSymlink == os.ModeSymlink {
 				// Create path verifies the symlink or fails if it is not what we wanted.
-				_, err = c.createPath(path, false, sec)
+				_, err = c.createPath(path, true, sec)
 			} else {
 				err = fmt.Errorf("cannot create symlink in %q: existing file in the way", path)
 			}
