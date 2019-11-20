890c8a1dfebedc1f62525cd9cee8b0a730271d7a
Zygmunt Krynicki <me@zygoon.pl>
2019-11-20 18:15:45 +0100

    many: use transient scope instead of pids cgroup for app tracking
    This needs a much better commit message
    
    Signed-off-by: Zygmunt Krynicki <me@zygoon.pl>

--- overlord/snapstate/snapstate_test.go
+++ overlord/snapstate/snapstate_test.go
@@ -920,12 +920,9 @@ func (s snapmgrTestSuite) TestInstallFailsOnBusySnap(c *C) {
 		}
 		return info, nil
 	})
-	mockPidsCgroupDir := c.MkDir()
-	restore := snapstate.MockPidsCgroupDir(mockPidsCgroupDir)
-	defer restore()
 
 	// And with cgroup v1 information indicating the app has a process with pid 1234.
-	writePids(c, filepath.Join(mockPidsCgroupDir, "snap.some-snap.app"), []int{1234})
+	writePids(c, filepath.Join(dirs.CgroupDir, "irrelevant", "snap.$RANDOM.some-snap.app.scope"), []int{1234})
 
 	// Attempt to install revision 2 of the snap.
 	snapsup := &snapstate.SnapSetup{
@@ -978,8 +975,8 @@ func (s snapmgrTestSuite) TestInstallDespiteBusySnap(c *C) {
 		}
 		return info, nil
 	})
-	// And with cgroup v1 information indicating the app has a process with pid 1234.
-	writePids(c, filepath.Join(dirs.PidsCgroupDir, "snap.some-snap.app"), []int{1234})
+	// And with cgroup information indicating the app has a process with pid 1234.
+	writePids(c, filepath.Join(dirs.CgroupDir, "irrelevant", "snap.$RANDOM.some-snap.app.scope"), []int{1234})
 
 	// Attempt to install revision 2 of the snap.
 	snapsup := &snapstate.SnapSetup{
