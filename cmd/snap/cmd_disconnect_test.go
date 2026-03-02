b090eaddc3372d486d16d04cb23a6cedcbaf9ac5
Zygmunt Krynicki <zygmunt.krynicki@canonical.com>
2026-03-03 00:13:09 +0100

    cmd/snap: use testutil.MockEnv in disconnect test

--- cmd/snap/cmd_disconnect_test.go
+++ cmd/snap/cmd_disconnect_test.go
@@ -22,12 +22,12 @@ package main_test
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/jessevdk/go-flags"
 	. "gopkg.in/check.v1"
 
 	. "github.com/snapcore/snapd/cmd/snap"
+	"github.com/snapcore/snapd/testutil"
 )
 
 func (s *SnapSuite) TestDisconnectHelp(c *C) {
@@ -228,8 +228,7 @@ func (s *SnapSuite) TestDisconnectCompletion(c *C) {
 			c.Fatalf("unexpected path %q", r.URL.Path)
 		}
 	})
-	os.Setenv("GO_FLAGS_COMPLETION", "verbose")
-	defer os.Unsetenv("GO_FLAGS_COMPLETION")
+	defer testutil.MockEnv(map[string]string{"GO_FLAGS_COMPLETION": "verbose"})()
 
 	expected := []flags.Completion{}
 	parser := Parser(Client())
